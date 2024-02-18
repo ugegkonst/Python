a585892fe79493d94ad88f1f51d777403d5dde4a
Egor Ugnivenko <ugegkonst@yandex.ru>
2024-02-18 15:36:57 +0300

    employee new employees

--- employee.py
+++ employee.py
@@ -1,31 +1,65 @@
 class Employee:
     vacation_days = 28
-# remaining_vacation_days = None
+
+    def __init__(self, first_name, second_name, gender):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.gender = gender
+        self.remaining_vacation_days = Employee.vacation_days
+
+    def consume_vacation(self, days):
+        self.remaining_vacation_days -= days
+
+    def get_vacation_details(self):
+        return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
+
+
+class FullTimeEmployee(Employee):
+
+    def __init__(self, first_name, second_name, gender):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.gender = gender
+        self.remaining_vacation_days = Employee.vacation_days
+
+    def consume_vacation(self, days):
+        self.remaining_vacation_days -= days
+
+    def get_vacation_details(self):
+        return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
+
+    def get_unpaid_vacation(self, beginning_date: str, day_quantity: int):
+        return f'Начало неоплачиваемого отпуска: {beginning_date}, \
+            продолжительность: {day_quantity} дней.'
+
+
+class PartTimeEmployee(Employee):
+    vacation_days = 14
 
     def __init__(self, first_name, second_name, gender):
         self.first_name = first_name
         self.second_name = second_name
         self.gender = gender
         self.remaining_vacation_days = self.vacation_days
-# Сюда добавьте новый атрибут remaining_vacation_days
 
-    def consume_vacation(self, days_gone):
-
-# self.remaining_vacation_days = Employee.vacation_days
-        self.remaining_vacation_days -= days_gone
+    def consume_vacation(self, days):
+        self.remaining_vacation_days -= days
 
     def get_vacation_details(self):
         return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
 
-    # Сюда добавьте методы consume_vacation и get_vacation_details.
+# Расширьте класс Employee, создав классы FullTimeEmployee и PartTimeEmployee.
 
-# Пример использования класса:
+# Пример использования:
 
 
-employee = Employee('Роберт', 'Крузо', 'м')
-employee.consume_vacation(12)
-print(employee.get_vacation_details())
+full_time_employee = FullTimeEmployee('Роберт', 'Крузо', 'м')
 
-employee1 = Employee('John', 'Smith', 'м')
-employee1.consume_vacation(7)
-print(employee1.get_vacation_details())
+full_time_employee.consume_vacation(7)
+print(full_time_employee.get_vacation_details())
+
+print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
+part_time_employee = PartTimeEmployee('Алёна', 'Пятницкая', 'ж')
+
+
+print(part_time_employee.get_vacation_details())
